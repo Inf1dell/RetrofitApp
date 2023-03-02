9e94afa7d0f488065d75ec689b3b683c443e4b38
Insaf Khisamiev <khisamiev.insaf@gmail.com>
2023-03-02 22:23:38 +0300

    v2

--- app/src/main/java/apk/karmak/retrofitapp/MainActivity.java
+++ app/src/main/java/apk/karmak/retrofitapp/MainActivity.java
@@ -23,76 +23,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl("https://medic.madskill.ru/api/")
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build();
-//
-//        MyAPI myAPI = retrofit.create(MyAPI.class);
-//
-//        Call<DataModel> call = myAPI.getMessage();
-//
-//        call.enqueue(new Callback<DataModel>() {
-//            @Override
-//            public void onResponse(Call<DataModel> call, Response<DataModel> response) {
-//                if(response.code() != 200) {
-//                    Log.e("", "Check");
-//                    return;
-//                }
-//
-//                Log.e("MSG", response.body().toString());
-//                Log.e("MSG", response.body().getMessage());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<DataModel> call, Throwable t) {
-//
-//            }
-//        });
-
-        postData("Ivan@iiii@re");
-    }
-
-    private void postData(String name) {
-
-        // below line is for displaying our progress bar.
-
-        // on below line we are creating a retrofit
-        // builder and passing our base url
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://medic.madskill.ru/api/")
-                // as we are sending data in json format so
-                // we have to add Gson converter factory
                 .addConverterFactory(GsonConverterFactory.create())
-                // at last we are building our retrofit builder.
                 .build();
-        // below line is to create an instance for our retrofit api class.
-        MyAPI retrofitAPI = retrofit.create(MyAPI.class);
 
-        // passing data from our text fields to our modal class.
-        DataModel modal = new DataModel("ivan@iii@ru");
-
-        // calling a method to create a post and passing our modal class.
-        Call<DataModel> call = retrofitAPI.getMes(modal);
-
-        // on below line we are executing our method.
+        MyAPI getWeatherApi = retrofit.create(MyAPI.class);
+        Call<DataModel> call = getWeatherApi.getMess("its@worked.yes");
         call.enqueue(new Callback<DataModel>() {
             @Override
             public void onResponse(Call<DataModel> call, Response<DataModel> response) {
-                // this method is called when we get response from our api.
-                Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
-
-
                 Log.e("MSG", response.code()+"");
-//                Log.e("MSG", responseFromAPI.getMessage());
+                Log.e("MSG", response.body().getMessage()+"");
             }
 
             @Override
             public void onFailure(Call<DataModel> call, Throwable t) {
-                // setting text to our text view when
-                // we get error response from API.
-                Log.e("MSG", "Error found is : " + t.getMessage());
+                Log.e("MSG", t.getMessage());
             }
         });
+
+//        postData();
     }
+
+//    private void postData() {
+//
+//
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl("https://medic.madskill.ru/api/")
+//
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//        MyAPI retrofitAPI = retrofit.create(MyAPI.class);
+//
+//        DataModel modal = new DataModel("ivan@iii@ru");
+//        Call<DataModel> call = retrofitAPI.getMes(modal);
+//
+//        call.enqueue(new Callback<DataModel>() {
+//            @Override
+//            public void onResponse(Call<DataModel> call, Response<DataModel> response) {
+//                Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
+//
+//
+//                Log.e("MSG", response.code()+"");
+////                Log.e("MSG", responseFromAPI.getMessage());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<DataModel> call, Throwable t) {
+//                Log.e("MSG", "Error found is : " + t.getMessage());
+//            }
+//        });
+//    }
 }
--- app/src/main/java/apk/karmak/retrofitapp/MyAPI.java
+++ app/src/main/java/apk/karmak/retrofitapp/MyAPI.java
@@ -2,17 +2,23 @@ package apk.karmak.retrofitapp;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface MyAPI {
 
+//    @Headers({"email: its@worked.why"})
 //    @POST("sendCode")
-//    Call<DataModel> getMessage();
+//    Call<DataModel> getMes(@Body DataModel dataModel);
 
     @Headers({"email: its@worked.why"})
     @POST("sendCode")
-    Call<DataModel> getMes(@Body DataModel dataModel);
+    Call<DataModel> getMess(@Header("email") String email);
+
+
 
 }
